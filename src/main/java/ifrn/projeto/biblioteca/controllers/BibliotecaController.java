2fb5d9a905dd18739966e673700fc11c5c6d4900
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-10-20 14:56:02 -0300

    Criando Repository para evento

--- src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
+++ src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
@@ -1,13 +1,18 @@
 package ifrn.projeto.biblioteca.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.projeto.biblioteca.models.Emprestimo;
+import ifrn.projeto.biblioteca.repositories.EmprestimoRepository;
 
 @Controller
 public class BibliotecaController {
+	
+	@Autowired
+	private EmprestimoRepository er;
 	
 	@RequestMapping("/emprestimos/form")
 	public String form() {
@@ -18,6 +23,7 @@ public class BibliotecaController {
 	public String realizar(Emprestimo emprestimo) {
 		
 		System.out.println(emprestimo);
+		er.save(emprestimo);
 		
 		return "emprestimo-realizado";
 	}
---
+++ src/main/java/ifrn/projeto/biblioteca/repositories/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+package ifrn.projeto.biblioteca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ifrn.projeto.biblioteca.models.Emprestimo;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
+
+}
