63db30204dce025a403a990031b77835d51f5524
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-10-20 14:43:24 -0300

    Eviando dados do formulário do empréstimo para a aplicação

--- src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
+++ src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
@@ -1,7 +1,10 @@
 package ifrn.projeto.biblioteca.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.projeto.biblioteca.models.Emprestimo;
 
 @Controller
 public class BibliotecaController {
@@ -11,4 +14,11 @@ public class BibliotecaController {
 		return "formEmprestimo";
 	}
 
+	@PostMapping("/emprestimos")
+	public String realizar(Emprestimo emprestimo) {
+		
+		System.out.println(emprestimo);
+		
+		return "emprestimo-realizado";
+	}
 }
