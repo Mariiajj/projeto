178c6f5e03230191a70fb790c48e13e67f69adf9
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-10-22 11:53:21 -0300

    Criando a listagem dos empréstimos

--- src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
+++ src/main/java/ifrn/projeto/biblioteca/controllers/BibliotecaController.java
@@ -1,30 +1,43 @@
 package ifrn.projeto.biblioteca.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.projeto.biblioteca.models.Emprestimo;
 import ifrn.projeto.biblioteca.repositories.EmprestimoRepository;
 
 @Controller
+@RequestMapping("/emprestimos")
 public class BibliotecaController {
 	
 	@Autowired
 	private EmprestimoRepository er;
 	
-	@RequestMapping("/emprestimos/form")
+	@RequestMapping("/form")
 	public String form() {
 		return "biblioteca/formEmprestimo";
 	}
 
-	@PostMapping("/emprestimos")
+	@PostMapping
 	public String realizar(Emprestimo emprestimo) {
 		
 		System.out.println(emprestimo);
 		er.save(emprestimo);
 		
 		return "biblioteca/emprestimo-realizado";
+	}
+	
+	@GetMapping
+	public ModelAndView listar() {
+		List<Emprestimo> emprestimos = er.findAll();
+		ModelAndView mv = new ModelAndView("biblioteca/lista");
+		mv.addObject("emprestimos", emprestimos);
+		return mv;
 	}
 }
