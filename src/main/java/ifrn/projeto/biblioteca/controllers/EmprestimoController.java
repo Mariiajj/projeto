44ed504f883439fe002cabf24bc5192d1ee6e32a
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-11-04 14:12:37 -0300

    Refazendo e consertando erros no projeto

--- src/main/java/ifrn/projeto/biblioteca/controllers/EmprestimoController.java
+++ src/main/java/ifrn/projeto/biblioteca/controllers/EmprestimoController.java
@@ -1,18 +1,20 @@
 package ifrn.projeto.biblioteca.controllers;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import ifrn.projeto.biblioteca.models.Aluno;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ifrn.projeto.biblioteca.models.Emprestimo;
 import ifrn.projeto.biblioteca.models.Livro;
 import ifrn.projeto.biblioteca.repositories.AlunoRepository;
@@ -35,29 +37,55 @@ public class EmprestimoController {
 		return "biblioteca/formEmprestimo";
 	}
 
-	@PostMapping
-	public String realizarEmprestimo(@Validated Emprestimo emprestimo, BindingResult result, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return "biblioteca/formEmprestimo";
-		}
-        
-		Aluno aluno = al.findByMatricula(emprestimo.getMatricula().getMatricula());
-	    Livro livro = lr.findByTitulo(emprestimo.getTitulo().getTitulo());
-	    if (livro == null) {
-	        result.rejectValue("titulo.titulo", "error.titulo", "Erro: Livro não encontrado");
-	        return form(emprestimo);
+    @PostMapping  
+	public String realizarEmprestimo(String titulo, String matricula, Emprestimo emprestimo, BindingResult result, RedirectAttributes attributes) {
+    	
+    	if (result.hasErrors()) {
+	        return "biblioteca/formEmprestimo";
 	    }
 	    
-		return "redirect:/emprestimos/form";
-	}
-	
-    
-	@GetMapping
-	public ModelAndView listarEmprestimos() {
-		List<Emprestimo> emprestimos = ep.findAll();
-		ModelAndView mv = new ModelAndView("biblioteca/listaEmprestimo");
-		mv.addObject("emprestimos", emprestimos);
-		return mv;
-	}
+	    System.out.println(titulo);
+	    System.out.println(matricula);
+	    
+	    ep.save(emprestimo);
+	    attributes.addFlashAttribute("mensagem", "Empréstimo salvo com sucesso!");
+	    
+	    Livro livro = emprestimo.getLivro();
+	    if (livro == null) {
+	    	result.rejectValue("livro.titulo", "error.titulo", "O livro não foi selecionado.");
+	        return "biblioteca/formEmprestimo";
+	    }
 
+	    Livro livroIdentificado = lr.findByTitulo(emprestimo.getLivro().getTitulo());
+
+	    if (livroIdentificado == null) {
+	        result.rejectValue("livro.titulo", "error.titulo", "O cadastro do livro não foi encontrado no sistema.");
+	        return "biblioteca/formEmprestimo";
+	    }
+	    
+	    Aluno aluno = emprestimo.getAluno();
+	    
+	    if (aluno == null) {
+	        result.rejectValue("aluno.matricula", "error.matricula", "O aluno(a) não foi selecionado.");
+	        return "biblioteca/formEmprestimo";
+	    }
+
+	    Aluno alunoEncontrado = al.findByMatricula(aluno.getMatricula());
+
+	    if (alunoEncontrado == null) {
+	        result.rejectValue("aluno.matricula", "error.matricula", "O aluno(a) não possui cadastro no sistema.");
+	        return "biblioteca/formEmprestimo";
+	    }
+	   
+	    return "redirect:/emprestimos/form";  
+	}  
+	
+        /*@GetMapping
+    	public String listarEmprestimos() {
+    		
+    		List<Emprestimo> emprestimos = ep.findAll();
+    		
+    	}*/
+    		
 }
+
